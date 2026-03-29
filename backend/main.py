c1ea5bc751bcee438008f30f3995ca295b1b7bd8
Geranguli <berezonka33@gmail.com>
2026-03-29 04:13:56 +0300

    Добавлена конфигурация БД с SQLite и SQLAlchemy

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from routers import users, boards
+from database import engine, Base
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Kanban"}
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(users.router)
 app.include_router(boards.router)
