eeb35f6ae62fec1b6c29611e52429d3f630caa79
Geranguli <berezonka33@gmail.com>
2026-03-29 04:21:51 +0300

    Добавлена модель User для БД

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routers import users, boards
 from database import engine, Base
+from models import User
 
 app = FastAPI()
 
---
+++ backend/models.py
@@ -0,0 +1,9 @@
+from database import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
