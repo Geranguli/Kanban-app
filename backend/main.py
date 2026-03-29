ecd47e21f22aed15e1a6de58e04ff51a1d694ff6
Geranguli <berezonka33@gmail.com>
2026-03-29 04:07:55 +0300

    Добавлен роутер досок с endpoint /boards

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import users
+from routers import users, boards
 
 app = FastAPI()
 
@@ -8,3 +8,4 @@ def read_root():
     return {"message": "Kanban"}
 
 app.include_router(users.router)
+app.include_router(boards.router)
