ed4b0f45f87458278223da8d9d3c8a8b0173dd09
Geranguli <berezonka33@gmail.com>
2026-03-30 16:59:24 +0300

    Добавлен CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
-from routers import users, boards, columns, cards
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
 from models import User, Board, KanbanColumn, Card
+from routers import boards, users, columns, cards
 
 app = FastAPI()
+
+# запросы с фронтенда 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
