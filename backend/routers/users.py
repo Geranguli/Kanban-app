6b648c6fc4e567ff122d90b7685c2cd862b4305d
Geranguli <berezonka33@gmail.com>
2026-03-30 05:33:09 +0300

    Добавлены эндпоинты входа и получения пользователя по id

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -1,7 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from database import get_db
+from models import User
+from schemas import UserCreate, UserResponse
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
 
-@router.post("/login")
-def login(username: str):
-    return {"id": 1, "username": username}
+# если пользователь с таким именем уже есть - возвращаем его, если нет - создаём нового
+@router.post("/login", response_model=UserResponse)
+def login(user: UserCreate, db: Session = Depends(get_db)):
+    stmt = select(User).where(User.username == user.username)
+    db_user = db.scalars(stmt).first()
+    if not db_user:
+        db_user = User(username=user.username)
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+    return db_user
+
+@router.get("/{user_id}", response_model=UserResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    stmt = select(User).where(User.id == user_id)
+    db_user = db.scalars(stmt).first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+    return db_user
